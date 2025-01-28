e7f7349660eb6b00d355a7f017cc42886e8dda14
tjsm <mathincosmos@gmail.com>
2025-01-28 11:07:45 +0530

    setup redis over cloud

--- utils/config.js
+++ utils/config.js
@@ -9,7 +9,7 @@ const SHARE_SECRET = process.env.SHARE_SECRET;
 const WSIZE = 20 * 1000; // in sec
 const RLIMIT = 10; //  api request in ws
 const ALIMIT = 3; //  authentication attempts in ws
-
+const REDIS_URL = process.env.REDIS_URL;
 module.exports = {
   WSIZE,
   RLIMIT,
@@ -20,4 +20,5 @@ module.exports = {
   STORAGE_URL,
   API_KEY,
   SHARE_SECRET,
+  REDIS_URL,
 };
--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,4 +1,6 @@
+const { REDIS_URL } = require('./config');
+
 const Redis = require('ioredis');
-const redis = new Redis();
+const redis = new Redis(REDIS_URL);
 
 module.exports = redis;
